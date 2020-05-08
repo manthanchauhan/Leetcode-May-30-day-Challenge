fd07818dd2d8b56c650b33214daa89e726fe423d
manthanchauhan913 <manthanchauhan913@gmail.com>
2020-05-08 22:21:08 +0530

    Division by zero handled

--- Check-If-It-Is-a-Straight-Line.cc
+++ Check-If-It-Is-a-Straight-Line.cc
@@ -4,6 +4,13 @@ public:
         int n = coordinates.size();
         
         if (n == 2) return true;
+
+	if (coordinates[1][0] == coordinates[0][0]) {
+	    int x = coordinates[0][0];
+	    
+	    for (int i = 1; i < n; i ++) if (coordinates[i][0] != x) return false;
+	    return true;
+	}
         
         float slope = float(coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]);
         float c = coordinates[0][1] - slope*coordinates[0][0];
